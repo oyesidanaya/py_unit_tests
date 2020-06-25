6859d9d4c183c09ec6c2941c6a7d1d5ee3a99973
Yesid Anaya <oyesidanaya@gmail.com>
2020-06-25 15:00:29 -0500

    Unit tests for Area calculation version 1

---
+++ src/area.py
@@ -0,0 +1,9 @@
+def area_square(side):
+    return side * side
+
+def area_rectangle(base,high):
+    return base * high
+
+def area_circle(radio):
+    return 3.1416 * radio ** 2
+
---
+++ tests/test_area.py
@@ -0,0 +1,17 @@
+import unittest
+from src import area
+
+class TestArea(unittest.TestCase):
+
+    def test_area_square(self):
+        self.assertEqual(4, area.area_square(2))
+
+    def test_area_rectangle(self):
+        self.assertEqual(20, area.area_rectangle(5, 4))
+
+    def test_area_circle(self):
+        self.assertAlmostEqual(12.56, area.area_circle(2.0), delta=0.01)
+
+
+if __name__ == '__main__':
+    unittest.main()

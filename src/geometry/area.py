4dd766f12790c3b66ad30e2601fea3879a8e02d8
Yesid Anaya <oyesidanaya@gmail.com>
2020-08-11 15:35:26 -0500

    Add math.pi constant, remove pytest-cov for coverage library

--- src/geometry/area.py
+++ src/geometry/area.py
@@ -1,11 +1,12 @@
+import math
 def area_square(side):
     return side * side
 
 
-def area_rectangle(base,high):
+def area_rectangle(base, high):
     return base * high
 
 
 def area_circle(radio):
-    return 3.1416 * radio ** 2
+    return math.pi * radio ** 2
 
